e6e0640e5a8c90b1d879db2e2c4c6aabc389700a
IgnaxA <ignat.panoff@gmail.com>
2024-09-14 18:35:29 +0500

    add: added impl of parsing file from api but its doesnt work

--- server/src/rest/dtos/input/impls/file-dto.ts
+++ server/src/rest/dtos/input/impls/file-dto.ts
@@ -2,15 +2,15 @@ import {RestBody} from "../rest-body";
 import {Request} from "express";
 
 export class FileDto extends RestBody<FileDto> {
-    private _file: Buffer | null;
+    private _file: Buffer;
 
-    public get file(): Buffer | null {
+    public get file(): Buffer {
         return this._file;
     }
 
     constructor() {
         super();
-        this._file = null;
+        this._file = Buffer.alloc(0);
     }
 
     public parse(req: Request): this {
---
+++ server/src/utils/file-utils.ts
@@ -0,0 +1,6 @@
+
+export class FileUtils {
+    public static TEXT_FILE_FORMAT: string = "utf8";
+    public static TEXT_FILE_FORMAT_BUFFER: BufferEncoding = "utf-8";
+
+}
